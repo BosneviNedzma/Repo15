5b5757ab4fde277e33985be537924a989b0b36f8
BosneviNedzma <brkicnedzma@gmail.com>
2021-09-28 02:54:54 +0200

    () => to Write Concise Anonymous Functions

---
+++ ex06/src/index.js
@@ -0,0 +1,8 @@
+// Only change code below this line
+const arena = function() {
+    // Only change code above this line
+    "use strict";
+    return new Date();
+};
+console.log(arena());
+module.exports = arena;
